005706713df455605410f0997d530bc30245c124
Benoît Piégu <benoit.piegu@inra.fr>
2018-04-13 16:31:55 +0200

    corrige bug sortie

--- g2tir_tabDynamik.pl
+++ g2tir_tabDynamik.pl
@@ -269,11 +269,9 @@ sub findPairsMotif {
 
 	my $dist = distance($R1_uc,$R2_uc);
 	if ( $dist <= 1){
-	  print $fh join ("\t", $chr, $pStart, $nEndPos, $nEndPos- $pStart, $dist);
-	  if ($motifPrint){
-	    print $fh join ("\t", $R1, $R2);
-	  }
-	  print $fh "\n";
+	  my @out = ($chr, $pStart, $nEndPos, $nEndPos- $pStart, $dist);
+	  push @out,  ($R1, $R2) if $motifPrint;
+	  print $fh join ("\t", @out), "\n";
 	}
       }
     }
